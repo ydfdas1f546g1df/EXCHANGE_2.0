879d0c8f33b195000166ab545a002b8d8a65d238
Raffael Schaeren <gamecheckboss@hotmail.com>
2023-12-29 23:55:00 +0100

    Finally

--- src/MTAC/main.cpp
+++ src/MTAC/main.cpp
@@ -10,15 +10,11 @@ void moveNewFiles(const fs::path& sourcePath, const fs::path& destinationPath) {
 
     while (true) {
         for (const auto& entry : fs::directory_iterator(sourcePath)) {
-            // Überprüfen, ob es sich um ein reguläres File handelt
             if (fs::is_regular_file(entry)) {
-                // Dynamischen Teil des Dateinamens extrahieren (username)
                 std::string username = entry.path().stem().string();
 
-                // Ziel-Pfad erstellen (Zielverzeichnis + Dateiname)
                 fs::path destinationFile = destinationPath / username / entry.path().filename();
 
-                // Datei verschieben
                 fs::rename(entry.path(), destinationFile);
 
                 std::cout << "Datei erfolgreich verschoben von "
@@ -26,19 +22,15 @@ void moveNewFiles(const fs::path& sourcePath, const fs::path& destinationPath) {
             }
         }
 
-        // Eine Pause von 1 Minute (kann nach Bedarf angepasst werden)
         std::this_thread::sleep_for(std::chrono::minutes(1));
     }
 }
 
 int main() {
-    // Dynamisch gescannter Quellpfad
     fs::path sourcePath = "/var/{domain}/mailboxes/";
 
-    // Pfad zum Zielverzeichnis
     fs::path destinationPath = "/pfad/zum/zielverzeichnis/";
 
-    // Ziel-Pfad erstellen (Zielverzeichnis + Dateiname)
     moveNewFiles(sourcePath, destinationPath);
 
     return 0;
